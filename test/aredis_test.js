9f502fbe7a32e852826e0df651d692cf9a6cd32a
okunishinishi <okunishinishi@gmail.com>
2019-01-27 10:17:43 +0900

    Version incremented to 1.0.3

--- test/aredis_test.js
+++ test/aredis_test.js
@@ -5,8 +5,7 @@
 'use strict'
 
 const Aredis = require('../lib/aredis.js')
-const assert = require('assert')
-const co = require('co')
+const assert = require('assert').strict
 
 describe('aredis', () => {
   before(async () => {
@@ -18,22 +17,28 @@ describe('aredis', () => {
   })
 
   it('Aredis', async () => {
-    let aredis = new Aredis()
+    const aredis = new Aredis()
     await aredis.set('testing:foo', 'bar')
-    let foo = await aredis.get('testing:foo')
+    const foo = await aredis.get('testing:foo')
     assert.equal(foo, 'bar')
 
     await aredis.del('testing:baz')
     await aredis.hset('testing:baz', 'quz', 'quzz')
-    let baz = await aredis.hgetall('testing:baz')
+    const baz = await aredis.hgetall('testing:baz')
     assert.deepEqual(baz, { quz: 'quzz' })
+
+    assert.deepEqual(await aredis.hkeys('testing:baz'), ['quz'])
+    assert.equal(await aredis.hget('testing:baz', 'quz'), 'quzz')
 
     await aredis.hdel('testing:baz', 'quz')
     await aredis.hset('testing:baz', 'zoo', 'zuu')
-    let baz2 = await aredis.hgetall('testing:baz')
+    const baz2 = await aredis.hgetall('testing:baz')
     assert.deepEqual(baz2, { zoo: 'zuu' })
-    
+
     await aredis.del('testing:baz')
+
+    await aredis.hmset('x', 'a', 1, 'b', 2)
+    assert.deepEqual(await aredis.hgetall('x'), { a: '1', b: '2' })
 
     aredis.quit()
   })
